4f1637f2474c5fcefae1fc893b654151da1f1148
crendu <rendu.clement@gmail.com>
2018-06-19 21:37:13 +0200

    création des classes noeuds et feuilles

--- Exercice4_CompressionImage/Exercice4_CompressionImage/QuadTree.cpp
+++ Exercice4_CompressionImage/Exercice4_CompressionImage/QuadTree.cpp
@@ -1,7 +1,10 @@
 #include "stdafx.h"
 #include "QuadTree.h"
 
-/*
-QuadTree::QuadTree() {}
-QuadTree::~QuadTree() {}
-*/
+
+// Forward declarations (so that the class names can be used in QuadTree)
+template <typename T> class QuadLeaf; // The type of a QuadTree leaf
+template <typename T> class QuadNode; // The type of a QuadTree node
+
+template <typename T> QuadTree::QuadTree() {}
+template <typename T> QuadTree::~QuadTree() {}
