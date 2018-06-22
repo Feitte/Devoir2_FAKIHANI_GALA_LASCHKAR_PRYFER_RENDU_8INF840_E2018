3663130e416b8f8e088f82e2ef81c1d7dde1a643
BenjaminLaschkar <benjamin.laschkar@devinci.fr>
2018-06-22 12:33:45 -0400

    Final push, everything good

--- Exercice3_Labyrinthes/Exercice3_Labyrinthes/cell.cpp
+++ Exercice3_Labyrinthes/Exercice3_Labyrinthes/cell.cpp
@@ -8,8 +8,8 @@ bool Cell::visit(deque<Cell *> & path) {
 		return false;
 	visited = true; // mark as visited
 	cout << "visiting cell " << number << endl;
-	if (number == 1) {
-		cout << "Maze entirely explored\n";
+	if (stopping) {
+		cout << "Maze explored until node :" << number << endl;
 		return true;
 	}
 	// put neighbors into deque
@@ -20,5 +20,31 @@ bool Cell::visit(deque<Cell *> & path) {
 		if (!(*start)->visited)
 			//path.push_front(*start); // depth-first
 			path.push_back (*start); // breadth-first
+	return false;
+}
+
+bool Cell::visitAStar(deque<Cell *> & path) {
+	if (visited) // already been here
+		return false;
+	visited = true; // mark as visited
+	cout << "visiting cell " << number << endl;
+	if (stopping) {
+		cout << "Maze explored until node :" << number << endl;
+		return true;
+	}
+	// put neighbors into deque
+	list <Cell *>::iterator start, stop;
+	start = neighbors.begin();
+	stop = neighbors.end();
+	Cell * choosing_node_to_push = NULL;
+	
+	for (; start != stop; ++start)
+		if (!(*start)->visited) {
+			(*start)->path_cost = path_cost + 1;
+			actual_cost = (*start)->heuristic + path_cost;
+			cout << heuristic << "__" << path_cost << endl;
+			path.push_front(*start);
+		}
+	
 	return false;
 }
--- Exercice3_Labyrinthes/Exercice3_Labyrinthes/cell.h
+++ Exercice3_Labyrinthes/Exercice3_Labyrinthes/cell.h
@@ -6,14 +6,18 @@ using namespace std;
 
 class Cell {
 public:
-	Cell(int c, int n, int h) : code(c), number(n), heuristic(h), visited(false) { }
+	Cell(int c, int n, int h, bool s) : code(c), number(n), heuristic(h), stopping(s), visited(false) { }
 	void addNeighbor(Cell * n) { neighbors.push_back(n); }
 	bool visit(deque<Cell *> &);
+	bool visitAStar(deque<Cell *> & path);
 	int code;
 	int number;
 	int heuristic;
-	int cost;
+	int actual_cost = 0; //infinite
+	int path_cost = 0;
+	bool stopping = false;
 protected:
 	bool visited;
 	list <Cell *> neighbors;
+	
 };
